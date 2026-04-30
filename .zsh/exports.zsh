723a50749609a88f5006c3839519a2d19db0e807
muizz <contact@abdulmuizz.dev>
2026-04-30 12:20:20 +0800

    update zshrc 30-04-2026

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,3 +1,4 @@
 export TERM=xterm-256color
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.cargo/bin
+export FZF_DEFAULT_OPTS="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
