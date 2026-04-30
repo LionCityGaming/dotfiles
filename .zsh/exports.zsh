f050983c10c81732cd8b55a44ea024418fde0343
muizz <contact@abdulmuizz.dev>
2026-04-30 11:58:53 +0800

    update zshrc 30-04-2026

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,4 +1,4 @@
 export TERM=xterm-256color
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.cargo/bin
-export EZA_COLORS="da=36:di=34;1:ex=32;1:fi=37:ln=35;1:or=31;1:*.zip=33:*.tar=33:*.gz=33:*.mp4=35:*.mkv=35:*.mp3=36:*.flac=36:*.jpg=33:*.png=33:*.pdf=31:*.yml=36:*.yaml=36:*.json=36:*.toml=36:*.sh=32:*.py=32:*.js=32:*.ts=32:*.md=37:*.txt=37"
+export EZA_COLORS="da=38;5;244:di=38;5;33;1:ex=38;5;40;1:fi=38;5;255:ln=38;5;213;1:or=38;5;196;1:*.zip=38;5;220:*.tar=38;5;220:*.gz=38;5;220:*.mp4=38;5;141:*.mkv=38;5;141:*.mp3=38;5;81:*.flac=38;5;81:*.jpg=38;5;214:*.png=38;5;214:*.gif=38;5;214:*.pdf=38;5;196:*.yml=38;5;122:*.yaml=38;5;122:*.json=38;5;122:*.toml=38;5;122:*.sh=38;5;40:*.py=38;5;40:*.js=38;5;40:*.ts=38;5;40:*.md=38;5;255:*.txt=38;5;255"
