b5fc6fd0ca485ce67b3c90310d68e1e1ef53ca37
muizz <contact@abdulmuizz.dev>
2026-04-30 11:52:35 +0800

    update zshrc 30-04-2026

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,3 +1,4 @@
 export TERM=xterm-256color
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.cargo/bin
+export EZA_COLORS="da=36:di=34;1:ex=32;1:fi=37:ln=35;1:or=31;1:*.zip=33:*.tar=33:*.gz=33:*.mp4=35:*.mkv=35:*.mp3=36:*.flac=36:*.jpg=33:*.png=33:*.pdf=31:*.yml=36:*.yaml=36:*.json=36:*.toml=36:*.sh=32:*.py=32:*.js=32:*.ts=32:*.md=37:*.txt=37"
